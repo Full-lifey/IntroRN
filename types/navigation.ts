cf87bca50aa657ea7e22d670afe24c68891d5f24
Joel Perez <joel@joelperez.dev>
2023-10-12 12:35:41 -0600

    add nested prop types

--- types/navigation.ts
+++ types/navigation.ts
@@ -1,12 +1,23 @@
 import type { NavigatorScreenParams } from '@react-navigation/native';
 import type { ColorPalette } from '../types';
+import type { CompositeScreenProps } from '@react-navigation/native';
+
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+
 
 export type MainStackParamList = {
-  Home: undefined;
+  Home: { newPalette?: ColorPalette };
   ColorPalette: ColorPalette;
 };
 
 export type RootStackParamList = {
-  MainStack: NavigatorScreenParams<MainStackParamList>;
+  Main: NavigatorScreenParams<MainStackParamList>;
   AddNewPaletteModal: undefined;
 };
+
+export type RootStackScreenProps = NativeStackScreenProps<RootStackParamList>
+
+export type MainStackScreenProps = CompositeScreenProps<
+  NativeStackScreenProps<MainStackParamList, 'Home'>,
+  NativeStackScreenProps<RootStackParamList>
+>;
